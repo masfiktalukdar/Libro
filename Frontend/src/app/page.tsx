ebe195ce67f780df27c9e78aefcb4bd792113bf7
Masfik Talukdar <freelancermasfik@gmail.com>
2026-04-25 23:03:35 +0600

    initial steup and route intialization

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='flex justify-center text-2xl'>Landing Page Coming</div>
+  )
+}
+
+export default LandingPage
---
+++ Frontend/src/components/auth/AuthNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AuthNav() {
+  return (
+    <div className="flex ">
+      <div>Left Side</div>
+      <div>Right Side</div>
+    </div>
+  );
+}
+
+export default AuthNav
