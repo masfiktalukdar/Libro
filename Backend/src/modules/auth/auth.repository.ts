28344ab820a136cf5a59c661744dc684bf7d3a36
Masfik Talukdar <freelancermasfik@gmail.com>
2026-06-09 22:52:48 +0600

    auth.repository.ts file created for managing most of the task for authentication

--- Backend/src/modules/auth/auth.repository.ts
+++ Backend/src/modules/auth/auth.repository.ts
@@ -1,11 +1,61 @@
 import { dbPool } from "@config/dbConnect.js";
 import {
   UserEntity,
-  RegesterUserDTO,
   InstitutionMemberEntity,
-  AddInstitutionMemberDTO,
 } from "@modules/auth/auth.interface.js";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
+import { Pool, PoolConnection } from "mysql2/promise";
 
-// export class AuthRepository {
-//   async createUser(user: UserEntity): Promise<UserEntity> {}
-// }
+export class AuthRepository {
+  async createUser(user: UserEntity): Promise<UserEntity> {
+    const createUserSQL = `
+      INSERT INTO user(user_id, full_name, user_email, user_phone, user_password_hashed, gender, avatar_url) VALUES (?,?,?,?,?,?,?)
+    `;
+    await dbPool.execute<ResultSetHeader>(createUserSQL, [
+      user.user_id,
+      user.full_name,
+      user.user_email,
+      user.user_phone,
+      user.user_password_hashed,
+      user.gender,
+      user.avatar_url,
+    ]);
+
+    return user;
+  }
+
+  async findEmail(email: string): Promise<UserEntity | null> {
+    const findEmailSQL = `
+      SELECT * FROM 
+      user WHERE user_email = ? AND deleted_at IS NULL 
+      LIMIT 1
+    `;
+
+    const [emails] = await dbPool.execute<RowDataPacket[]>(findEmailSQL, [
+      email.toLocaleLowerCase().trim(),
+    ]);
+    if (email.length === 0) return null;
+    return emails[0] as UserEntity;
+  }
+
+  async createInstitutionMember(
+    institutionMember: InstitutionMemberEntity,
+    connectionTarget?: Pool | PoolConnection,
+  ): Promise<InstitutionMemberEntity> {
+    const connectionRunner = connectionTarget || dbPool;
+    const createInstitutionMemberSQL = `
+      INSERT INTO institution_member (institution_member_id, institution_id, user_id) 
+      VALUES(?,?,?)
+    `;
+
+    await connectionRunner.execute(createInstitutionMemberSQL, [
+      institutionMember.institution_member_id,
+      institutionMember.institution_id,
+      institutionMember.user_id,
+    ]);
+
+    return institutionMember;
+  }
+}
+
+export const authRepository = new AuthRepository();
