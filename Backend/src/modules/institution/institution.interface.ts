a79f6a75f5db1802525ef246ab32159c771d79b1
Masfik Talukdar <freelancermasfik@gmail.com>
2026-06-14 10:58:47 +0600

    institute repository has been created

---
+++ Backend/src/modules/institution/institution.interface.ts
@@ -0,0 +1,46 @@
+export type InstitutionType = "university" | "polytechnic";
+export type StudentApprovalSystem = "manual" | "automatic";
+export type MembershipFeeType =
+  | "none"
+  | "per_month"
+  | "per_semester"
+  | "per_year";
+
+export interface InstitutionEntity {
+  institution_id: string;
+  institution_name: string;
+  institution_short_form: string;
+  institution_slug: string;
+  institution_logo_url?: string | null;
+  institution_email: string;
+  institution_founding_year: number;
+  institution_eiin_number: string;
+  institution_location: string;
+  institution_type: InstitutionType;
+  student_approval_system: StudentApprovalSystem;
+  membership_fee_type: MembershipFeeType;
+  membership_fee_amount: number;
+  student_book_borrow_limit: number;
+  student_fine_limit_amount: number;
+  reservation_expiry_in_minutes: number;
+  library_opening_time: string;
+  library_closing_time: string;
+  created_at: Date | string;
+  updated_at: Date | string;
+  deleted_at?: Date | string | null;
+}
+
+export type InstitutionRegistrationRequstEntity = Omit<
+  InstitutionEntity,
+  | "institution_short_form"
+  | "institution_slug"
+  | "student_approval_system"
+  | "membership_fee_amount"
+  | "membership_fee_type"
+  | "student_book_borrow_limit"
+  | "student_fine_limit_amount"
+  | "reservation_expiry_in_minutes"
+  | "reservation_expiry_in_minutes"
+  | "library_opening_time"
+  | "library_closing_time"
+>;
